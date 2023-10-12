8f4ae5ae30068d873489fb80bb63269b4827f0e5
Rushaway <rushaway@hotmail.fr>
2023-10-12 11:14:37 +0200

    curl 8.4.0

---
+++ curl/tests/libtest/CMakeFiles/lib1552.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/tmp/curl/lib/timediff.c" "/tmp/curl/build/tests/libtest/CMakeFiles/lib1552.dir/__/__/lib/timediff.c.o"
+  "/tmp/curl/tests/libtest/first.c" "/tmp/curl/build/tests/libtest/CMakeFiles/lib1552.dir/first.c.o"
+  "/tmp/curl/tests/libtest/lib1552.c" "/tmp/curl/build/tests/libtest/CMakeFiles/lib1552.dir/lib1552.c.o"
+  "/tmp/curl/tests/libtest/testutil.c" "/tmp/curl/build/tests/libtest/CMakeFiles/lib1552.dir/testutil.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_CONFIG_H"
+  "LIB1552"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../lib"
+  "lib"
+  "include"
+  "../tests/libtest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/tmp/curl/build/lib/CMakeFiles/libcurl_shared.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
